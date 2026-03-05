c4c1702bd45fdb2b2d56bf5cf7fc1814cac36c7d
Luckyboy Maliehe <luckyboymaliehe@gmail.com>
2026-03-05 21:30:11 +0200

    Sort dropdown changes

--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -7,65 +7,64 @@ import Card from "../card/card.component";
 import AddCard from "../add-card/add-card.component";
 import "./card-list.styles.css";
 
-
 const CardList = () => {
   const { ideas } = useIdeas();
-  const [sortType, setSortType] = useState<'newest' | 'oldest' | 'az' | 'za'>('oldest');
-  const [sortedIdeas, setSortedIdeas] = useState<ideaType[]>()
+  const [sortType, setSortType] = useState<"newest" | "oldest" | "az" | "za">(
+    "oldest",
+  );
+  const [sortedIdeas, setSortedIdeas] = useState<ideaType[]>();
 
- const sortingIdeas = () => {
+  const sortingIdeas = () => {
+    setSortedIdeas(
+      [...ideas].sort((a, b) => {
+        if (sortType === "newest") return b.modifiedDate - a.modifiedDate;
+        if (sortType === "oldest") return a.modifiedDate - b.modifiedDate;
+        if (sortType === "az") return a.title.localeCompare(b.title);
+        if (sortType === "za") return b.title.localeCompare(a.title);
+        return 0;
+      }),
+    );
+  };
 
-   setSortedIdeas([...ideas].sort((a, b) => {
-   if (sortType === 'newest') return b.modifiedDate - a.modifiedDate;
-   if (sortType === 'oldest') return a.modifiedDate - b.modifiedDate;
-   if (sortType === 'az') return a.title.localeCompare(b.title);
-   if (sortType === 'za') return b.title.localeCompare(a.title);
-   return 0;
-  }));
- } 
-
-
-useEffect(()=>{
+  useEffect(() => {
     setSortedIdeas(ideas);
   }, [ideas]);
 
-useEffect(()=>{
+  useEffect(() => {
     sortingIdeas();
   }, [sortType]);
 
-const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
-  const {value} = e.target;
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
 
-  if(value === 'newest' || value === 'oldest' ||value === 'az' || value === 'za') {
-
-    setSortType(value);
-  }
-}
+    if (
+      value === "newest" ||
+      value === "oldest" ||
+      value === "az" ||
+      value === "za"
+    ) {
+      setSortType(value);
+    }
+  };
 
   return (
     <div className="card-list">
       <div className="sort-container">
         <select className="dropdown" value={sortType} onChange={handleChange}>
-          <option value='newest'>Newest</option>
-          <option value='oldest'>Oldest</option>
-          <option value='az'>A-Z</option>
-          <option value='za'>Z-A</option>
+          <option value="newest">Newest</option>
+          <option value="oldest">Oldest</option>
+          <option value="az">A-Z</option>
+          <option value="za">Z-A</option>
         </select>
       </div>
 
-    <div className="card-list-container">
-      {sortedIdeas &&
-        sortedIdeas.map((idea, idx) => {
-          return (
-            <Card
-              key={idx}
-              card={idea}
-              idx={idx}
-            />
-          );
-        })}
-      <AddCard />
-    </div>
+      <div className="card-list-container">
+        {sortedIdeas &&
+          sortedIdeas.map((idea, idx) => {
+            return <Card key={idx} card={idea} idx={idx} />;
+          })}
+        <AddCard />
+      </div>
     </div>
   );
 };
